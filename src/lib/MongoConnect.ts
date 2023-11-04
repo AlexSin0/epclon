ad6e7f9acecc5ad72d6d9fe37631752823e0621e
Alex Synelnikov <alex.synelnikov@gmail.com>
2023-11-04 21:23:18 +0200

    Connected database to authentication

--- src/lib/MongoConnect.ts
+++ src/lib/MongoConnect.ts
@@ -1,8 +1,9 @@
 import { MongoClient } from "mongodb";
-import { env } from "process";
 
 const uri = process.env.MONGO_CONNECTION_STRING!;
 
 const client = new MongoClient(uri);
 
 export const db = client.db("EpclonDB");
+
+export const userCollection = db.collection("Users");
