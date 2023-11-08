0a19b6a935a88a2b6eb21ea0e8a110543b766528
Goose <arabadzhi.artem@gmail.com>
2023-11-09 01:13:26 +0200

    Added search, temp registration and login pages.

---
+++ src/app/temp/login/page.tsx
@@ -0,0 +1,12 @@
+
+export default function LoginPage() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <p>Login/Email</p>
+      <p>Password</p>
+      <p>GitHub</p>
+      <p>Google</p>
+      <p>Register</p>
+    </main>
+  );
+}
