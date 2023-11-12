326e2cb25a950eb02c5c5f066e31f41b2ac005fc
Alex Synelnikov <alex.synelnikov@gmail.com>
2023-11-12 17:01:33 +0200

    Added profile icon

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,20 +1,15 @@
 export default function SearchBar() {
   return (
     <div className="flex justify-between px-3 py-2">
-      <form
-        id="searchBar"
-        className=" p-0.5 bg-white border-solid rounded-full h-11 flex justify-between"
-      >
+      <form className="p-0.5 bg-white border-solid rounded-full h-11 flex justify-between">
         <input
-          id="searchInput"
           className="rounded-full pl-2 text-black"
           type="text"
           placeholder="Search"
         />
         <button
-          id="searchButton"
           className="bg-[#21ad9a] float-right w-10 h-10 rounded-full text-white"
-          type="button"
+          type="submit"
         >
           Go
         </button>
