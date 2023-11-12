69fc79570a0461a0ad7e1cc0455b7cf581b87b50
Alex Synelnikov <alex.synelnikov@gmail.com>
2023-11-12 17:56:10 +0200

    Profile button fix

--- src/components/ProfileButton.tsx
+++ src/components/ProfileButton.tsx
@@ -9,63 +9,47 @@ export default function ProfileButton() {
 
   const [clickState, setClickState] = useState<boolean>();
 
-  if (!session) {
-    return (
-      <div>
-        <Image
-          src="/placeholder.jpg"
-          alt="Avatar"
-          className="dark:invert border-solid rounded-full hower:w-[60px]"
-          width={50}
-          height={50}
-          onClick={() => setClickState(!clickState)}
-        />
-        {clickState ? (
-          // TODO: Refactor
-          <div className="absolute right-5 z-10 mt-2 w-47 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <p className="block px-4 py-2 text-sm text-gray-700">Guest</p>
-            <hr/>
-            <button className="block px-4 py-2 text-sm text-gray-700" onClick={() => signIn()}>
-              Sign in
-            </button>
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
-    );
-  }
+  const menuPanel = session ? (
+    <div className="absolute right-5 z-10 mt-2 w-47 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5">
+      <p className="block px-4 py-2 text-sm text-gray-700">
+        {session.user?.name}
+      </p>
+      <hr />
+      <button
+        className="block px-4 py-2 text-sm text-gray-700"
+        onClick={() => signOut()}
+      >
+        Sign out
+      </button>
+
+      <Link href="/profile" className="block px-4 py-2 text-sm text-gray-700">
+        Your Profile
+      </Link>
+    </div>
+  ) : (
+    <div className="absolute right-5 z-10 mt-2 w-47 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+      <p className="block px-4 py-2 text-sm text-gray-700">Guest</p>
+      <hr />
+      <button
+        className="block px-4 py-2 text-sm text-gray-700"
+        onClick={() => signIn()}
+      >
+        Sign in
+      </button>
+    </div>
+  );
 
   return (
     <div>
       <Image
-        src={session?.user?.image!}
-        alt="Avatar"
-        className="dark:invert border-solid rounded-full hower:w-[60px]"
+        src={session?.user?.image ?? "/logo.jpg"}
+        alt="User profile icon"
+        className="rounded-full"
         width={50}
         height={50}
         onClick={() => setClickState(!clickState)}
-      {clickState && (
-        <div className="absolute right-5 z-10 mt-2 w-47 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5">
-          <p className="block px-4 py-2 text-sm text-gray-700">
-            {session.user?.name}
-          </p>
-          <hr/>
-          <button
-            className="block px-4 py-2 text-sm text-gray-700"
-            onClick={() => signOut()}
-          >
-            Sign out
-          </button>
-
-          <Link
-            href="/profile"
-            className="block px-4 py-2 text-sm text-gray-700"
-          >
-            Your Profile
-          </Link>
-        </div>
-      )}
+      />
+      {clickState && menuPanel}
     </div>
   );
 }
