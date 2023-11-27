d3de0d578519d0ed2e83d742274464c41d51211c
ZdravstvuiteDenis <den860850@gmail.com>
2023-11-27 15:11:04 +0200

    Added more props classes

--- src/types/ItemProps.ts
+++ src/types/ItemProps.ts
@@ -3,7 +3,7 @@ export class ItemProps {
   public brand?: "intel" | "amd";
 }
 
-export type Socket = "LGA 1151" | "LGA 1155";
+export type Socket = "LGA 1151" | "LGA 1155" | "AM5" | "AM4";
 
 export class CpuProps extends ItemProps {
   public socket?: Socket;
@@ -16,7 +16,25 @@ export class MotherboardProps extends ItemProps {
   //...
 }
 
-//ram class
-//gpu class
-//motherboard class
+export type PCI_version = "PCIe 1.0" | "PCIe 2.0" | "PCIe 3.0" | "PCIe 4.0" | "PCIe 5.0";
+
+export class GpuProps extends ItemProps {
+  public gpumemory_Mb?: number;
+  public gpumemory_MHz?: number;
+  public gpumodel?: string;
+  public gpumemory_type?: string;
+  public pcie?: PCI_version;
+}
+
+export class HardDriveProps extends ItemProps {
+  public harddrivememory_Gb?: number;
+  public readspeed_MBs?: number;
+  public writespeed_MBs?: number;
+  public harddrivetype?: string;
+  public harddriveintrface?: string;
+}
+
+//ram class Vasa
+//gpu class 
+//motherboard class Vasa
 //hard-drive class
