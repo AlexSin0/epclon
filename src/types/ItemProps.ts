85c2745d40544dd77aaebca4188ebfa8ce9f1fc3
Alex Synelnikov <alex.synelnikov@gmail.com>
2023-11-28 13:59:47 +0200

    ItemProps fixes

--- src/types/ItemProps.ts
+++ src/types/ItemProps.ts
@@ -1,69 +1,77 @@
 export class ItemProps {
-  public color?: "none" | "black" | "white";
-  public brand?: "intel" | "amd" | "Asus" | "MSI" | "Adata"| "Corsair";
+  public color?:
+    | "None"
+    | "Black"
+    | "White"
+    | "Grey"
+    | "Green"
+    | "Red"
+    | "Blue"
+    | "Yellow";
+  public brand?: "Intel" | "AMD" | "Asus" | "MSI" | "Adata" | "Corsair";
 }
 
 export type Socket = "LGA 1151" | "LGA 1155" | "AM5" | "AM4";
-
-export type RamType = "DDR5" | "DDR4" | "DDR3";
-
-export type GpuCableType = "6-pin" | "8-pin" | "6+2-pin";
-
-export type PCI_version = "PCIe 1.0" | "PCIe 2.0" | "PCIe 3.0" | "PCIe 4.0" | "PCIe 5.0";
+export type RamStandard = "DDR5" | "DDR4" | "DDR3";
+export type GpuCableType = "None" | "6-pin" | "8-pin" | "6+2-pin";
+export type PCIeVersion =
+  | "PCIe 1.0"
+  | "PCIe 2.0"
+  | "PCIe 3.0"
+  | "PCIe 4.0"
+  | "PCIe 5.0";
 
 export class CpuProps extends ItemProps {
   public socket?: Socket;
   public powerUsage_W?: number;
-  public clockspeed_GHz?: number; 
+  public clockspeed_GHz?: number;
   public model?: string;
-  public thread?: number; 
-  public coresNumber?: number;
+  public threadCount?: number;
+  public coreCount?: number;
 }
 
 export class MotherboardProps extends ItemProps {
   public socket?: Socket;
-  public ramType?: RamType;
-  public pcie?: PCI_version;
-  public ramSlots?: number;
+  public ramStandard?: RamStandard;
+  public pcieVersion?: PCIeVersion;
+  public ramSlotCount?: number;
   public powerUsage_W?: number;
   public model?: string;
+  public type?: "ATX" | "Micro-ATX" | "Mini-ITX";
 }
 
 export class RamProps extends ItemProps {
-  public ramType?: RamType;
+  public ramStandard?: RamStandard;
   public frequency_GHz?: number;
-  public memory_Gb?: number;
+  public ramCapacity_Gb?: number;
   public powerUsage_W?: number;
-  public model?: string;
 }
 
 export class PowerUnitProps extends ItemProps {
   public gpuCableType?: GpuCableType;
   public power_W?: number;
-  public model?: string;
+  public efficiencyCertificate?:
+    | "80 Plus"
+    | "80 Plus Bronze"
+    | "80 Plus Silver"
+    | "80 Plus Gold"
+    | "80 Plus Platinum"
+    | "80 Plus Titanium";
 }
 
 export class GpuProps extends ItemProps {
-  public pcie?: PCI_version;
-  public cableType?: GpuCableType;
-  public memory_Mb?: number;
-  public clockspeed_MHz?: number;
+  public pcieVersion?: PCIeVersion;
+  public gpuCableType?: GpuCableType;
+  public memoryCapacity_Gb?: number;
   public model?: string;
-  public memory_type?: string;
+  public memoryStandard?: string;
 }
 
 export class HardDriveProps extends ItemProps {
-  public memory_Gb?: number;
+  public memoryCapacity_Gb?: number;
   public readSpeed_MBs?: number;
   public writeSpeed_MBs?: number;
-  public type?: string;
-  public intrface?: string;
+  public type?: "HDD" | "SSD";
+  public intrface?: "SATA 3.0" | "NVMe" | "M.2 SATA";
   public powerUsage_W?: number;
-  public model?: string;
 }
-
-//ram class Vasa
-//gpu class 
-//motherboard class Vasa
-//hard-drive class
-
