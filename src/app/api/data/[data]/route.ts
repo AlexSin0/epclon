23143df9794a0bd4e5d451482f58534658b576f1
Alex Synelnikov <alex.synelnikov@gmail.com>
2023-12-15 18:31:39 +0200

    Added CRUD API

--- src/app/api/data/[data]/route.ts
+++ src/app/api/data/[data]/route.ts
@@ -8,9 +8,31 @@ export async function GET(
   const collName = snakeToCamel(params.data);
   const data = await db.collection(collName).find().toArray();
 
-  console.log(collName);
-
   return NextResponse.json(data);
+}
+
+export async function POST(
+  request: Request,
+  { params }: { params: { data: string } }
+) {
+  const collName = snakeToCamel(params.data);
+  const collection = db.collection(collName);
+
+  const data = await request.json();
+
+  if (!data) {
+    return NextResponse.error();
+  }
+
+  const dbResponce = await collection.insertOne(data);
+
+  if (dbResponce.acknowledged) {
+    return NextResponse.json(
+      await collection.findOne({ _id: dbResponce.insertedId })
+    );
+  }
+
+  return NextResponse.error();
 }
 
 function snakeToCamel(input: string): string {
