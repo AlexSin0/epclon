661a357e9cdd4c0b0b296d6c952af87c7bd5802d
Alex Synelnikov <alex.synelnikov@gmail.com>
2023-12-19 21:26:40 +0200

    Added payment system

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,7 @@
+import Checkout from "@/components/Checkout";
 import CatalogItem from "@/components/catalog/CatalogItem";
 import { GetCatalogById, GetUserLiked } from "@/lib/Catalog";
+import { Purchase } from "@/lib/Payment";
 import { ObjectId } from "mongodb";
 import { getServerSession } from "next-auth";
 import { cookies } from "next/headers";
@@ -26,41 +28,38 @@ export default async function Cart() {
 
   return (
     <main className="flex bg-zinc-400 ">
-      <div className="p-4 px-12 w-full grid grid-cols-4 gap-4">
-        {catalog.length === 0 ? (
-          <p
-            className="
+      {catalog.length === 0 ? (
+        <p
+          className="
           text-white text-lg text-center py-5
           bg-slate-600 rounded h-[10vh] w-[25vh]
           absolute left-[45vw] top-[40vh]
           "
-          >
-            Cart is empty
-          </p>
-        ) : (
-          catalog.map((item, index) => (
-            <CatalogItem
-              item={item}
-              key={index}
-              isLiked={email ? likedStr.includes(item._id.toString()) : null}
-              isInBasket={basketArr.includes(item._id.toString())}
-            />
-          ))
-        )}
-      </div>
-      <div
-        className="w-[25%]
+        >
+          Cart is empty
+        </p>
+      ) : (
+        <>
+          <div className="p-4 w-full grid grid-cols-4 gap-4">
+            {catalog.map((item, index) => (
+              <CatalogItem
+                item={item}
+                key={index}
+                isLiked={email ? likedStr.includes(item._id.toString()) : null}
+                isInBasket={basketArr.includes(item._id.toString())}
+              />
+            ))}
+          </div>
+          <div
+            className="w-[30%]
       text-white text-lg text-center
-      bg-slate-600 min-h-[10vh]
+      min-h-[10vh]
       sticky top-[40vh]"
-      >
-        <div>
-          <p>Total: {`${total.toFixed(2)}₴`}</p>
-          <button className="bg-emerald-600 py-1 m-1 w-[80%] rounded-xl static">
-            Buy {catalog.length} items
-          </button>
-        </div>
-      </div>
+          >
+            <Checkout amount={total} />
+          </div>
+        </>
+      )}
     </main>
   );
 }
--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useSession } from "next-auth/react";
 import React from "react";
+
+import { Purchase } from "@/lib/Payment";
 
 export default function PaymentForm({ amount }: { amount: number }) {
   const stripe = useStripe();
   const elements = useElements();
+
+  const session = useSession();
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -29,8 +34,14 @@ export default function PaymentForm({ amount }: { amount: number }) {
         payment_method: { card: cardElement },
       });
 
+      const formData = new FormData(e.target as HTMLFormElement);
+
+      console.log(formData.get("email"));
+
       if (cardResponse.paymentIntent?.status === "succeeded") {
         alert("Payment Successful");
+        Purchase(formData.get("email") as string);
+
         cardElement.clear();
       } else {
         alert("Payment Failed");
@@ -41,16 +52,39 @@ export default function PaymentForm({ amount }: { amount: number }) {
   };
 
   return (
-    <form onSubmit={onSubmit} className="p-2">
-      <h1 className="text-black text-center mb-4">
-        Payment {`${Number(amount).toFixed(2)}₴`}
-      </h1>
-      <CardElement />
+    <form
+      onSubmit={onSubmit}
+      className="p-2 flex flex-col justify-between h-full pb-14"
+    >
+      <div className="">
+        <input
+          required
+          name="email"
+          type="email"
+          placeholder="Email"
+          className="p-2 my-2 rounded-md w-full text-black"
+          defaultValue={session?.data?.user?.email ?? ""}
+        />
+        <input
+          required
+          name="name"
+          type="text"
+          placeholder="Name"
+          className="p-2 my-2 rounded-md w-full text-black"
+          defaultValue={session?.data?.user?.name ?? ""}
+        />
+      </div>
+      <div>
+        <h2 className="text-black text-center">Credit Card Details</h2>
+        <div className="p-4 my-2 rounded-md w-full bg-white">
+          <CardElement />
+        </div>
+      </div>
       <button
         type="submit"
         className="bg-blue-500 text-white w-full mt-8 p-3 rounded-md"
       >
-        Submit
+        Buy
       </button>
     </form>
   );
