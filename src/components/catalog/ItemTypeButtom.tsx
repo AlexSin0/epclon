112f29fe526c4429b0866cab9cd12f173e5de726
Alex Synelnikov <alex.synelnikov@gmail.com>
2023-12-19 22:14:48 +0200

    Categorized catalog

--- src/components/catalog/ItemTypeButtom.tsx
+++ src/components/catalog/ItemTypeButtom.tsx
@@ -1,10 +1,20 @@
-export default function ItemTypeButton({children}:{children: string;}){
-return(
-    <button className="text-white text-lg text-center
+import Link from "next/link";
+
+export default function ItemTypeButton({
+  productType,
+  children,
+}: {
+  productType: string;
+  children: string;
+}) {
+  return (
+    <Link
+      href={`/catalog/${productType}`}
+      className="text-white text-lg text-center
     rounded-md max-h-[100%] py-1
     hover:bg-white hover:bg-opacity-10"
     >
-    {children} 
-    </button>
-);
+      {children}
+    </Link>
+  );
 }
