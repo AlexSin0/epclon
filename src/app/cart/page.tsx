d9a05493ac66a1761bc2dcd8f95cb6f82a6c76bd
Alex Synelnikov <alex.synelnikov@gmail.com>
2024-12-27 19:46:04 +0200

    Decrement bought items quantity

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,65 +1,47 @@
 import Checkout from "@/components/payment/Checkout";
 import CatalogItem from "@/components/catalog/CatalogItem";
-import { GetCatalogById, GetUserLiked } from "@/lib/Catalog";
-import { Purchase } from "@/lib/Payment";
-import { ObjectId } from "mongodb";
+import { GetBasketItems, GetUserLiked } from "@/lib/Catalog";
 import { getServerSession } from "next-auth";
-import { cookies } from "next/headers";
 
 export default async function Cart() {
-  const session = await getServerSession();
-  const email = session?.user?.email;
+	const session = await getServerSession();
+	const email = session?.user?.email;
 
-  const liked = email ? await GetUserLiked(email) : [];
-  const likedStr = liked.map((x) => x.toString());
+	const liked = email ? await GetUserLiked(email) : [];
+	const likedStr = liked.map((x) => x.toString());
 
-  const basket = (await cookies()).get("basket")?.value;
-  const basketArr: string[] = basket ? JSON.parse(basket) : [];
+	const catalog = await GetBasketItems();
+	const total = catalog.reduce((acc, item) => acc + Number(item.cost), 0);
 
-  const basketIds: ObjectId[] = [];
-  basketArr.forEach((item) => {
-    try {
-      basketIds.push(new ObjectId(item));
-    } catch {}
-  });
-
-  const catalog = await GetCatalogById(basketIds);
-  const total = catalog.reduce((acc, item) => acc + Number(item.cost), 0);
-
-  return (
-    <main className="flex bg-zinc-400 ">
-      {catalog.length === 0 ? (
-        <p
-          className="
+	return (
+		<main className="flex bg-zinc-400 ">
+			{catalog.length === 0 ? (
+				<p
+					className="
           text-white text-lg text-center py-5
           bg-slate-600 rounded h-[10vh] w-[25vh]
           absolute left-[45vw] top-[40vh]
           "
-        >
-          Cart is empty
-        </p>
-      ) : (
-        <>
-          <div className="p-4 w-full grid grid-cols-4 gap-4">
-            {catalog.map((item, index) => (
-              <CatalogItem
-                item={item}
-                key={index}
-                isLiked={email ? likedStr.includes(item._id.toString()) : null}
-                isInBasket={basketArr.includes(item._id.toString())}
-              />
-            ))}
-          </div>
-          <div
-            className="w-[30%]
-      text-white text-lg text-center
-      min-h-[10vh]
-      sticky top-[40vh]"
-          >
-            <Checkout amount={total} />
-          </div>
-        </>
-      )}
-    </main>
-  );
+				>
+					Cart is empty
+				</p>
+			) : (
+				<>
+					<div className="p-4 w-full grid grid-cols-4 gap-4">
+						{catalog.map((item, index) => (
+							<CatalogItem
+								item={item}
+								key={index}
+								isLiked={email ? likedStr.includes(item._id.toString()) : null}
+								isInBasket={true}
+							/>
+						))}
+					</div>
+					<div className="w-[30%] text-white text-lg text-center min-h-[10vh] sticky top-[40vh]">
+						<Checkout amount={total} />
+					</div>
+				</>
+			)}
+		</main>
+	);
 }
